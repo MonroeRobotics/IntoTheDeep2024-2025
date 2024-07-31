7837a0850a616ff184af59cf0787aaaa4095c737
JEEF-THE-LEEF <jacob.dulin@gmail.com>
2024-07-30 22:33:23 -0400

    IT WORKS!!!
    it drives now, yipee

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive.java
@@ -22,8 +22,6 @@ public class drive extends OpMode{
     MecanumDrive drive;
     Vector2d position = new Vector2d(0,0);
     Pose2d pose = new Pose2d(position, 90);
-    Vector2d gamepadInput = new Vector2d(currentGamepad1.left_stick_x, currentGamepad1.left_stick_y);
-    PoseVelocity2d poseVelocity2d = new PoseVelocity2d(gamepadInput, currentGamepad1.right_stick_x);
 
     double drivePower =.8;
     double xPower;
@@ -59,11 +57,11 @@ public class drive extends OpMode{
             headingPower = 0;
         } else if (currentGamepad1.dpad_left) {
             xPower = 0;
-            yPower = -drivePower;
+            yPower = drivePower;
             headingPower = 0;
         } else if (currentGamepad1.dpad_right) {
             xPower = 0;
-            yPower = drivePower;
+            yPower = -drivePower;
             headingPower = 0;
         }
 
@@ -75,9 +73,15 @@ public class drive extends OpMode{
             drivePower = .8;
         }
 
+        Vector2d gamepadInput = new Vector2d(xPower, yPower);
+        PoseVelocity2d poseVelocity2d = new PoseVelocity2d(gamepadInput, headingPower);
+
         drive.setDrivePowers(poseVelocity2d);
         telemetry.addData("xPower", xPower);
         telemetry.addData("yPower", yPower);
         telemetry.addData("headingPower", headingPower);
+        telemetry.addData("leftStick x", currentGamepad1.left_stick_x);
+        telemetry.addData("lefStick y", currentGamepad1.left_stick_y);
+        telemetry.addData("rightStick x", currentGamepad1.right_stick_x);
         }
     }
