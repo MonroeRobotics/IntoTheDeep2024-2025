1fcd8c7404529e3d8608ae312a3467017238f59c
JEEF-THE-LEEF <jacob.dulin@gmail.com>
2024-08-28 21:52:06 -0400

    Swerve Work!
    dpad for directions as well as joystick. Currently Joystick does not function in the first quadrant.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/singleSwerveTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/singleSwerveTest.java
@@ -17,10 +17,10 @@ public class singleSwerveTest extends OpMode {
     //Double drivePower = .8;
     Gamepad currentGamepad;
     Gamepad previousGamepad;
-    Double driveAngle;
-    Double actualDriveRadian;
-    Double getActualDriveDegree;
-    @Override
+    double driveAngle;
+    double actualDriveRadian;
+    double getActualDriveDegree;
+    double multiplier =324;
     public void init() {
         telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
         heading = hardwareMap.get(Servo.class, "driveAngle");
@@ -40,27 +40,57 @@ public class singleSwerveTest extends OpMode {
             driveMotor.setPower(-drivePower);
         }
         else{driveMotor.setPower(0);}*/
-        if ((currentGamepad.right_stick_x >.1) || (currentGamepad.right_stick_y >.1) || (currentGamepad.right_stick_x <-.1) || (currentGamepad.right_stick_y <-.1)){
-            driveAngle = (Math.atan2(currentGamepad.right_stick_y, currentGamepad.right_stick_x));
+        if ((currentGamepad.right_stick_x >.1) || (currentGamepad.right_stick_y >.1) || (currentGamepad.right_stick_x <-.1) || (currentGamepad.right_stick_y <-.1) && !currentGamepad.x){
+            driveAngle = (Math.atan2(-currentGamepad.right_stick_y, currentGamepad.right_stick_x));
             actualDriveRadian = driveAngle;
-            driveAngle /= Math.PI;
+            driveAngle /= (Math.PI*2.0);
             getActualDriveDegree = driveAngle;
             if (driveAngle <0){
                 driveAngle+=1;
             }
+            driveAngle *= 360.0/multiplier;
+            driveAngle -=(.25*(360/multiplier));
             heading.setPosition(driveAngle);
-
-            //CR
+        }
+        if (currentGamepad.dpad_left){
+            driveAngle = (.25*(360.0/multiplier));
+            heading.setPosition(driveAngle);
+        }
+        if (currentGamepad.dpad_down){
+            driveAngle = (.5*(360.0/multiplier));
+            heading.setPosition(driveAngle);
+        }
+        if (currentGamepad.dpad_right){
+            driveAngle = (.75*(360.0/multiplier));
+            heading.setPosition(driveAngle);
+        }
+        if (currentGamepad.dpad_up){
+            driveAngle = (0);
+            heading.setPosition(driveAngle);
         }
 
+        if (currentGamepad.left_bumper){
+            heading.setPosition(0);
+        }
+        if (currentGamepad.right_bumper){
+            heading.setPosition(1);
+        }
+
+        if (currentGamepad.cross && !previousGamepad.cross){
+            multiplier-=1.0;
+        }
+        else if (currentGamepad.circle && !previousGamepad.circle){
+            multiplier+=1.0;
+        }
         //telemetry.addData("Motor Power", driveMotor.getPower());
         telemetry.addData("Status", "Running");
-        telemetry.addData("actualRad", actualDriveRadian);
+        telemetry.addData("input", actualDriveRadian);
         telemetry.addData("actualDeg", getActualDriveDegree);
         telemetry.addData("driveAngle", driveAngle);
         telemetry.addData("Servo Position", heading.getPosition());
         telemetry.addData("rightStickX", currentGamepad.right_stick_x);
-        telemetry.addData("rightStickY", currentGamepad.right_stick_y);
+        telemetry.addData("rightStickY", -currentGamepad.right_stick_y);
+        telemetry.addData("multiplier", multiplier);
         telemetry.update();
     }
 }
