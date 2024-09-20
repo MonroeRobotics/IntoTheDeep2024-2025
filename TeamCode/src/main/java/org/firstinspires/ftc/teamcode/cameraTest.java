275780a4b4e15abe3b486b488117d5a70a349d67
MonroeRobotics <144491371+MonroeRobotics@users.noreply.github.com>
2024-09-20 10:21:42 -0400

    Update cameraTest.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/cameraTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/cameraTest.java
@@ -5,7 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.vision.cameraThing;
 
-@TeleOp
+@TeleOp (name = "cameraTest", group = "Main")
 public class cameraTest extends OpMode {
 
     cameraThing camera;
