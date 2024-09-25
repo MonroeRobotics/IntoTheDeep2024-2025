74ba8d177310ee5ebe535c28af00ed7fdece3ba5
JEEF-THE-LEEF <jacob.dulin@gmail.com>
2024-09-25 13:29:06 -0400

    Create ServoRotate.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ServoRotate.java
@@ -0,0 +1,31 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.Gamepad;
+
+public class ServoRotate extends OpMode {
+
+    Gamepad gamepad;
+    CRServo crServo;
+
+    @Override
+    public void init() {
+        telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
+        crServo = hardwareMap.get(CRServo.class, "crServo");
+        gamepad = gamepad1;
+        crServo.setPower(0.0);
+    }
+
+    @Override
+    public void loop() {
+        if (gamepad.dpad_left){
+            crServo.setPower(1.0);
+        }
+        if (gamepad.dpad_right){
+            crServo.setPower(-1.0);
+        }
+    }
+}
