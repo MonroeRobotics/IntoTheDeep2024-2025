544be36ebace194fbe117f89fd614c333e853201
MonroeRobotics <144491371+MonroeRobotics@users.noreply.github.com>
2024-10-10 11:42:08 -0400

    Update drive.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive.java
@@ -7,9 +7,13 @@ import com.acmerobotics.roadrunner.PoseVelocity2d;
 import com.acmerobotics.roadrunner.Vector2d;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.Servo;
 
-import org.firstinspires.ftc.teamcode.driveClasses.MecanumDrive;
 import org.firstinspires.ftc.teamcode.vision.cameraThing;
 
 @TeleOp(name="drive", group="main")
@@ -24,7 +28,17 @@ public class drive extends OpMode{
     Gamepad currentGamepad2;
     Gamepad previousGamepad2;
 
-    MecanumDrive drive;
+    CRServo intake0;
+    CRServo intake1;
+    Servo intakeExtension;
+    Servo armAngleL;
+    Servo armAngleR;
+    Servo claw;
+    DcMotor leftSlide;
+    DcMotor rightSlide;
+
+
+    //MecanumDrive drive;
     Vector2d position = new Vector2d(0,0);
     Pose2d pose = new Pose2d(position, 90);
 
@@ -32,11 +46,17 @@ public class drive extends OpMode{
     double xPower;
     double yPower;
     double headingPower;
-    cameraThing cameraThing;
+
+    double intakeTarget;
+    int leftSlideTarget;
+    int rightSlideTarget;
+
+    double highBucketHeight;
+    //cameraThing cameraThing;
     @Override
     public void init() {
         telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
-        drive = new MecanumDrive(hardwareMap,pose);
+        //drive = new MecanumDrive(hardwareMap,pose);
 
         currentGamepad1 = new Gamepad();
         previousGamepad1 = new Gamepad();
@@ -48,8 +68,23 @@ public class drive extends OpMode{
         currentGamepad2 = gamepad2;
         previousGamepad2 = gamepad2;
 
-        cameraThing = new cameraThing(hardwareMap);
-        cameraThing.initCam();
+        //cameraThing = new cameraThing(hardwareMap);
+        //cameraThing.initCam();
+        leftSlide = hardwareMap.get(DcMotorEx.class, "leftSlide");
+        rightSlide = hardwareMap.get(DcMotorEx.class, "rightSlide");
+        intakeExtension = hardwareMap.get(Servo.class, "intakeExtension");
+
+        leftSlide.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        leftSlide.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightSlide.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        leftSlide.setPower(drivePower);
+        rightSlide.setPower(drivePower);
+
+        intakeTarget = intakeExtension.getPosition();
+        leftSlideTarget = leftSlide.getCurrentPosition();
+        rightSlideTarget = rightSlide.getCurrentPosition();
     }
 
     @Override
@@ -89,15 +124,43 @@ public class drive extends OpMode{
             drivePower = .8;
         }
 
+        if (currentGamepad2.dpad_left && !previousGamepad2.dpad_left){
+            intakeTarget +=.05;
+        }
+        if (currentGamepad2.dpad_right && !previousGamepad2.dpad_right){
+            intakeTarget -=.05;
+        }
+
+        if (currentGamepad2.dpad_up){
+            leftSlideTarget += 10;
+            rightSlideTarget +=10;
+        }
+        if (currentGamepad2.dpad_down){
+            leftSlideTarget -=10;
+            rightSlideTarget -=10;
+        }
+
         Vector2d gamepadInput = new Vector2d(xPower, yPower);
         PoseVelocity2d poseVelocity2d = new PoseVelocity2d(gamepadInput, headingPower);
 
-        drive.setDrivePowers(poseVelocity2d);
-        telemetry.addData("xPower", xPower);
+        leftSlide.setTargetPosition(leftSlideTarget);
+        rightSlide.setTargetPosition(rightSlideTarget);
+        leftSlide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        rightSlide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        intakeExtension.setPosition(intakeTarget);
+
+        //drive.setDrivePowers(poseVelocity2d);
+        /*telemetry.addData("xPower", xPower);
         telemetry.addData("yPower", yPower);
         telemetry.addData("headingPower", headingPower);
         telemetry.addData("leftStick x", currentGamepad1.left_stick_x);
         telemetry.addData("lefStick y", currentGamepad1.left_stick_y);
-        telemetry.addData("rightStick x", currentGamepad1.right_stick_x);
+        telemetry.addData("rightStick x", currentGamepad1.right_stick_x);*/
+        telemetry.addData("intake servo pos", intakeExtension.getPosition());
+        telemetry.addData("leftSlide Target", leftSlide.getTargetPosition());
+        telemetry.addData("leftSlide", leftSlide.getCurrentPosition());
+        telemetry.addData("rightSLide Target", rightSlide.getTargetPosition());
+        telemetry.addData("rightSlide", rightSlide.getCurrentPosition());
         }
     }
