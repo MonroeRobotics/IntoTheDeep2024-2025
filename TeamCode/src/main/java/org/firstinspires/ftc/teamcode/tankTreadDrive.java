a3c2b567a51bb70d29aaaebb16b8d7a1c9ec58d7
MonroeRobotics <144491371+MonroeRobotics@users.noreply.github.com>
2025-01-31 16:44:34 -0500

    tsa  bot drives

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tankTreadDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tankTreadDrive.java
@@ -1,24 +1,27 @@
 package org.firstinspires.ftc.teamcode;
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
-@TeleOp
-
+@TeleOp(name = "treadDrive", group = "Main")
 public class tankTreadDrive extends OpMode {
     double leftDrivePower;
     double rightDrivePower;
     double drivePower = 0.8;
-    Gamepad gamepad;
+    Gamepad currentGamepad1;
     DcMotorEx leftDriveMotor;
     DcMotorEx rightDriveMotor;
 
     @Override
     public void init() {
-        gamepad = new Gamepad();
+        telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
+        currentGamepad1 = new Gamepad();
         leftDriveMotor = hardwareMap.get(DcMotorEx.class, "leftDriveMotor");
         rightDriveMotor = hardwareMap.get(DcMotorEx.class, "rightDriveMotor");
 
@@ -26,16 +29,24 @@ public class tankTreadDrive extends OpMode {
         rightDriveMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
         leftDriveMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rightDriveMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
-        leftDriveMotor.setPower(drivePower);
-        rightDriveMotor.setPower(drivePower);
+        rightDriveMotor.setDirection(DcMotorSimple.Direction.REVERSE);
     }
 
     @Override
     public void loop() {
-        leftDrivePower = gamepad.left_stick_y + gamepad.left_stick_x;
-        rightDrivePower = gamepad.left_stick_y + (-1 * gamepad.left_stick_x);
+        if (currentGamepad1.left_stick_y >= .1 || currentGamepad1.left_stick_y <= .1 || currentGamepad1.left_stick_x >= .1 || currentGamepad1.left_stick_x <= .1 || currentGamepad1.right_stick_y >= .1 || currentGamepad1.right_stick_y <= .1 || currentGamepad1.right_stick_x >= .1 || currentGamepad1.right_stick_x <= .1) {
+            leftDrivePower = -currentGamepad1.left_stick_y + currentGamepad1.right_stick_x;
+            rightDrivePower = -currentGamepad1.left_stick_y + (-currentGamepad1.right_stick_x);
+        }
         leftDriveMotor.setPower(leftDrivePower);
         rightDriveMotor.setPower(rightDrivePower);
+
+        currentGamepad1.copy(gamepad1);
+
+        telemetry.addData("xpower", currentGamepad1.left_stick_x);
+        telemetry.addData("ypower", currentGamepad1.left_stick_y);
+        telemetry.update();
     }
 }
